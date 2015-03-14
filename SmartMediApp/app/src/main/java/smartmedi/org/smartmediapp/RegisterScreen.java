890819a1baf99fa7443047be1a360de504f4dc37
srinivasanviki <vikis_1@live.com>
2015-03-14 05:33:01 +0530

    updated the initial screen

---
+++ SmartMediApp/app/src/main/java/smartmedi/org/smartmediapp/RegisterScreen.java
@@ -0,0 +1,33 @@
+package smartmedi.org.smartmediapp;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import java.io.IOException;
+
+@EActivity(R.layout.activity_register_screen)
+public class RegisterScreen extends Activity {
+
+    @ViewById
+    EditText mobile_number;
+
+    @ViewById
+    TextView move_to_next;
+
+    @AfterViews
+    public void init() {
+        String text=mobile_number.getText().toString();
+        Typeface font=Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
+        move_to_next.setTypeface(font);
+    }
+}
