d158a0cc08513fd335e7899b740ef22337b501ec
prashanth9962 <prashanth.engr@gmail.com>
2015-03-17 04:12:32 +0530

    profile view layout

---
+++ SmartMediApp/app/src/main/java/smartmedi/org/smartmediapp/ProfileUpdater.java
@@ -0,0 +1,36 @@
+package smartmedi.org.smartmediapp;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_profile_updater)
+public class ProfileUpdater extends Activity {
+
+    @ViewById
+    RelativeLayout profile_updater;
+
+
+    @AfterViews
+    public   void init(){
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fontello.ttf");
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.profile_image_screen, null);
+        TextView image_upload= (TextView) view.findViewById(R.id.image_upload);
+        image_upload.setTypeface(typeface);
+        profile_updater.addView(view);
+    }
+
+
+}
